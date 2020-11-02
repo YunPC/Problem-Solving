3ccc8c316a948e01649912366f797d4b58c896c7
YunPC <46584354+YunPC@users.noreply.github.com>
2020-11-02 11:13:28 +0900

    P7 solved

--- P7/P7.cpp
+++ P7/P7.cpp
@@ -1,103 +1,49 @@
 #include <iostream>
-#include <vector>
-const int INF = 1000000000;
-using namespace std;
+#include <limits>
+using namespace std; 
 
-int min_time(vector<int>& pipe);
+#define N 500 
 
-int main()
-{
-    vector<int> pipe;
-    pipe.reserve(50000);
-    int n, len, len_sum, idx, time = 0;
-    cin >> n;
-    for(int i = 0; i < n; i++)
-    {
-        cin >> len;
-        pipe.push_back(len);
+int cost[N][N]; 
+bool v[N][N]; 
 
-    }
+int getMinTime(int i, int j, int* arr) 
+{ 
+	if (i == j) 
+		return 0; 
 
-    if(pipe.size() == 3)
-    {
-        int left[pipe.size()];
-        int left_sum = 0, right_sum = 0;
-        int gap = INF, idx;
-        int right[pipe.size()];
-        for(int i = 0; i < pipe.size(); i++)
-        {
-            left[i] = left_sum + pipe[i];
-            left_sum += pipe[i];
-        }
-        for(int i = pipe.size()-1; i > -1; i--)
-        {
-            right[i] = right_sum + pipe[i];
-            right_sum += pipe[i];
-        }
-        for(int i = 0; i < pipe.size()-1; i++)
-        {
-            if(abs(left[i] - right[i+1]) < gap)
-            {
-                idx = i;
-                gap = abs(left[i] - right[i+1]);
-            }
-        }
-        cout << left[idx]+right[idx+1];
+	if (v[i][j]) 
+		return cost[i][j]; 
 
-    }
-    else
-    {
-        cout << min_time(pipe);    
-    }
+	v[i][j] = 1; 
+	int& res = cost[i][j]; 
 
-    return 0;
-}
+	res = 1000000000; 
 
-int min_time(vector<int>& pipe)
-{
-    if(pipe.size() == 2)
-        return pipe[0]+pipe[1];
-    else if(pipe.size() == 1)
-        return 0;
-    else
-    {
-        int left[pipe.size()];
-        int left_sum = 0, right_sum = 0;
-        int gap = INF, idx;
-        int right[pipe.size()];
-        for(int i = 0; i < pipe.size(); i++)
-        {
-            left[i] = left_sum + pipe[i];
-            left_sum += pipe[i];
-        }
-        for(int i = pipe.size()-1; i > -1; i--)
-        {
-            right[i] = right_sum + pipe[i];
-            right_sum += pipe[i];
-        }
-        for(int i = 0; i < pipe.size()-1; i++)
-        {
-            if(abs(left[i] - right[i+1]) < gap)
-            {
-                idx = i;
-                gap = abs(left[i] - right[i+1]);
-            }
-        }
-        vector<int> left_v;
-        vector<int> right_v;
-        for(int i = 0; i <= idx; i++)
-        {
-            left_v.push_back(pipe[i]);
-        }
-        for(int i = idx + 1; i < pipe.size(); i++)
-        {
-            right_v.push_back(pipe[i]);
-        }
-        int left_res = min_time(left_v);
-        int right_res = min_time(right_v);
-        
-        return left[idx]+right[idx+1] + left_res + right_res;
+	int total = 0; 
+	for (int k = i; k <= j; k++) 
+		total += arr[k]; 
 
-    }
-    
-}
+	for (int k = i + 1; k <= j; k++) { 
+		res = min(res, total + getMinTime(i, k - 1, arr) 
+					+ getMinTime(k, j, arr)); 
+	} 
+
+	return res; 
+} 
+
+// Driver code 
+int main() 
+{ 
+	int arr[500]; 
+	int n, card_num; 
+	cin >> n;
+	for(int i = 0; i < n; i++)
+	{
+		cin >> card_num;
+		arr[i] = card_num;
+	}
+	cout << getMinTime(0, n - 1, arr); 
+
+	return 0; 
+} 
